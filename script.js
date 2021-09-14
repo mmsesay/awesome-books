385b6fcd9784ef9ee14f6492b63e38494673b5ca
mmsesay <maej_01@hotmail.com>
2021-09-14 01:21:41 +0000

    Refactor: booksArray to fetch data from the local storage Add: a function to iterate through the booksArray and append to the right dom element

--- script.js
+++ script.js
@@ -9,22 +9,37 @@ function Book(title, description) {
   this.description = description;
 }
 
-let booksArray = [];
-let newBookDiv  =  '';
+let booksArray = JSON.parse(localStorage.getItem('Books') || '[]');
+let newBookDiv = '';
+
+const showAllBooks = () => {
+  if (booksArray.length > 0) {
+    booksArray.forEach((book) => {
+      newBookDiv += `<p>${book.title}</p>
+        <p>${book.description}</p>
+        <button class='remove-book'>Remove</button>
+        <hr>`;
+    });
+  }
+
+  bookContentContainer.innerHTML = newBookDiv;
+};
+
+showAllBooks();
 
 function addBookToList(newBook) {
   // append the new book
-  newBookDiv += `<p>${newBook.title}</p>;
-    <p>${newBook.description}</p>;
-    <button class="remove-book">Remove</button>;
-    </hr>;`;
+  newBookDiv += `<p>${newBook.title}</p>
+    <p>${newBook.description}</p>
+    <button class='remove-book'>Remove</button>
+    <hr>`;
   bookContentContainer.innerHTML = newBookDiv;
   booksArray.push(newBook); // push the book to the array
-  localStorage.setItem("Books", JSON.stringify(booksArray)); // set the new book to the local storage
+  localStorage.setItem('Books', JSON.stringify(booksArray)); // set the new book to the local storage
   location.reload(); // relaod the window
 }
 
-bookForm.addEventListener("submit", (event) => {
+bookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const book = new Book(bookTitle.value, bookDescription.value);
   addBookToList(book);
